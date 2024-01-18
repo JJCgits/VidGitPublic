76acb57480f6cdacf7f73be2a013de1993aafddf
Joshua Chiyezhan <92063382+JJCgits@users.noreply.github.com>
2024-01-17 19:16:51 -0500

    Update imageCreator.py

--- imageCreator.py
+++ imageCreator.py
@@ -1,1 +1,16 @@
+import simple_image_download.simple_image_download as simp
+import Script
+def main():
+    script, scriptList = Script.main()
+    sList = scriptList.split(",")
+    print(sList)
+    
+    my_downloader = simp.Downloader()
 
+    for topic in sList:
+        try:
+            my_downloader.download(topic, limit=3)
+            my_downloader.extensions = '.jpg'
+        except:
+            pass
+    return(script, sList)
